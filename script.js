ae8f680c363bf922858456d9e8ff0f6fa6aa5a3f
Swapnaja27 <142212367+Swapnaja27@users.noreply.github.com>
2025-09-26 11:36:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let option=document.getElementById("colorSelect");
+let btn = document.querySelector("input");
+
+btn.addEventListener("click", () => {
+    const selectedValue = option.value;   
+    const selectedOption = option.options[option.selectedIndex]; // <option> element
+
+    // Hide the selected option
+    if (selectedValue !== "") {
+        selectedOption.style.display = "none";
+        option.selectedIndex = 0; // reset dropdown to default
+    }
+});
